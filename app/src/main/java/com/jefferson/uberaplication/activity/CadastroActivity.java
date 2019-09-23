27e62c95a9156f7e32591bcfd7295c206ba5047a
cabrall10 <jeffersoncabral_10@hotmail.com>
2019-09-23 16:02:27 -0300

    Cria classe Usuario

--- app/src/main/java/com/jefferson/uberaplication/activity/CadastroActivity.java
+++ app/src/main/java/com/jefferson/uberaplication/activity/CadastroActivity.java
@@ -3,14 +3,49 @@ package com.jefferson.uberaplication.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.jefferson.uberaplication.R;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private TextInputEditText campoNome, campoEmail, campoSenha;
+    private Switch switchTipoUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        campoNome = findViewById(R.id.editCadastroNome);
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        switchTipoUsuario = findViewById(R.id.switchTipoUsuario);
+
+    }
+
+    public void validarCadastroUsuario(){
+        String textoNome = campoNome.getText().toString();
+        String textoEmail = campoEmail.getText().toString();
+        String textoSenha = campoSenha.getText().toString();
+
+        if( !textoNome.isEmpty()){
+            if( !textoEmail.isEmpty()){
+                if( !textoSenha.isEmpty()){
+
+
+            }else{
+                Toast.makeText(CadastroActivity.this, "Preencha a senha!", Toast.LENGTH_SHORT).show();
+            }
+
+            }else{
+                Toast.makeText(CadastroActivity.this, "Preencha o e-mail!", Toast.LENGTH_SHORT).show();
+            }
+
+            }else{
+                Toast.makeText(CadastroActivity.this, "Preencha o nome!", Toast.LENGTH_SHORT).show();
+            }
     }
 }
