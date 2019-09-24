d2d495c8a74d9d71d9a2c5163a37993a901263c0
cabrall10 <jeffersoncabral_10@hotmail.com>
2019-09-24 15:42:52 -0300

    Trata exceções ao cadasrtar usuario no firebase

--- app/src/main/java/com/jefferson/uberaplication/activity/CadastroActivity.java
+++ app/src/main/java/com/jefferson/uberaplication/activity/CadastroActivity.java
@@ -14,6 +14,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.jefferson.uberaplication.R;
 import com.jefferson.uberaplication.config.ConfiguracaoFireBase;
 import com.jefferson.uberaplication.model.Usuario;
@@ -78,25 +81,46 @@ public class CadastroActivity extends AppCompatActivity {
 
                 if( task.isSuccessful() ){
 
-                    String idUsuario = task.getResult().getUser().getUid();
-                    usuario.setId( idUsuario );
-                    usuario.salvar();
+                    try{
+                        String idUsuario = task.getResult().getUser().getUid();
+                        usuario.setId( idUsuario );
+                        usuario.salvar();
 
-                    // Redireciona o usuário com base no seu tipo
-                    // Se o usuário for passageiro chama a activity
-                    // senão chama a activity requisicoes
-                    if( verificaTipoUsuario() == "P" ){
-                        startActivity(new Intent(CadastroActivity.this, MapsActivity.class));
-                        finish();
+                        // Redireciona o usuário com base no seu tipo
+                        // Se o usuário for passageiro chama a activity
+                        // senão chama a activity requisicoes
+                        if( verificaTipoUsuario() == "P" ){
+                            startActivity(new Intent(CadastroActivity.this, MapsActivity.class));
+                            finish();
 
-                        Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar Passageiro!", Toast.LENGTH_SHORT).show();
-                    }else{
-                        startActivity(new Intent(CadastroActivity.this, RequisicoesActivity.class));
-                        finish();
+                            Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar Passageiro!", Toast.LENGTH_SHORT).show();
+                        }else{
+                            startActivity(new Intent(CadastroActivity.this, RequisicoesActivity.class));
+                            finish();
 
-                        Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar Motorista!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar Motorista!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                }
+
+                }else{
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch ( FirebaseAuthWeakPasswordException e){
+                        excecao = "Digite uma senha mais forte";
+                    }catch ( FirebaseAuthInvalidCredentialsException e){
+                        excecao = "Por favor, digite um e-mail válido";
+                    }catch ( FirebaseAuthUserCollisionException e){
+                        excecao = "Esta conta já foi cadastrada";
+                    }catch ( Exception e){
+                        excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                        e.printStackTrace();
                     }
 
+                    Toast.makeText(CadastroActivity.this, excecao, Toast.LENGTH_SHORT).show();
                 }
 
             }
