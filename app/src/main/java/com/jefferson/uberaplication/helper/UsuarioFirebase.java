d5649f8ca5721d9f974c6005da1033d27bff4039
cabrall10 <jeffersoncabral_10@hotmail.com>
2019-09-24 17:44:43 -0300

    Implementa login do usuário

--- app/src/main/java/com/jefferson/uberaplication/helper/UsuarioFirebase.java
+++ app/src/main/java/com/jefferson/uberaplication/helper/UsuarioFirebase.java
@@ -1,5 +1,7 @@
 package com.jefferson.uberaplication.helper;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -9,9 +11,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.jefferson.uberaplication.activity.MapsActivity;
+import com.jefferson.uberaplication.activity.RequisicoesActivity;
 import com.jefferson.uberaplication.config.ConfiguracaoFireBase;
+import com.jefferson.uberaplication.model.Usuario;
 
-    public class UsuarioFirebase {
+public class UsuarioFirebase {
 
     public static FirebaseUser getUsuarioAtual(){
         FirebaseAuth usuario = ConfiguracaoFireBase.getFirebaseAutenticacao();
@@ -40,6 +49,34 @@ import com.jefferson.uberaplication.config.ConfiguracaoFireBase;
            e.printStackTrace();
            return false;
        }
+    }
+
+    public static void redirecionaUsuarioLogado(final Activity activity){
+        DatabaseReference usuariosRef = ConfiguracaoFireBase.getFirebaseDataBase()
+                .child( "usuarios" )
+                .child( getIdentificadorUsuario() );
+        usuariosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+
+                String tipoUsuario = usuario.getTipo();
+                if(tipoUsuario.equals("M")){
+                    activity.startActivity(new Intent(activity, RequisicoesActivity.class));
+                }else{
+                    activity.startActivity(new Intent(activity, MapsActivity.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public static String getIdentificadorUsuario(){
+        return getUsuarioAtual().getUid();
     }
 
 }
